6f52149d1b82648e91f6ec5ef668d72394165703
au781145 <au781145@uni.au.dk>
2026-03-12 14:21:16 +0100

    added GUI main.py

--- GUI/main.py
+++ GUI/main.py
@@ -1,2 +1,30 @@
-if __name__ == "__main__":
-	print("Heeeij")
+import tkinter as tk
+from tkinter import ttk
+
+class GUI:
+    def __init__(self): #__init__() svarer til en contructor i C++
+        self.root = tk.Tk()
+        self.root.geometry("320x180")
+        self.root.title("iSOUS")
+    def run(self):
+        self.root.mainloop()
+
+class Page: #klassen som laver Frames og skifter frame
+    def __init__(self,parent,app):
+        self.app = app
+        self.frame = tk.Frame(parent)
+
+    def show(self):
+        self.frame.tkraise()
+
+class HomePage(Page): #komposition
+    def __init__(self,parent,app):
+        super().__init__(parent,app)
+
+        title = tk.label(self.frame, text="iSOUS")
+        title.pack(pady=20)
+
+
+
+app = GUI()
+app.run()
