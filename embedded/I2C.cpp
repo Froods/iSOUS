1867427386015c3d7b4684218eabfcf2befcc369
Mads Sejerskilde <madssejerskilde2@gmail.com>
2026-03-26 16:50:27 +0100

    cleanup doxygen kommentering

--- embedded/I2C.cpp
+++ embedded/I2C.cpp
@@ -1,17 +1,9 @@
 #include "i2c.h"
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <avr/io.h>
-#include <avr/iom2560.h>
 #include <stdint.h>
 
 
-#define I2C_START 0x08
-#define I2C_REP_START 0x10
-#define I2C_MT_SLA_ACK 0x18
-#define I2C_MT_DATA_ACK 0x28
-#define I2C_MR_SLA_ACK 0x40
-#define I2C_MR_DATA_ACK 0x50
-#define I2C_MR_DATA_NACK 0x58
 
 void I2C::init_I2C(uint32_t clockFreq) {
   TWSR = 0x00; //No prescaler
@@ -20,31 +12,36 @@ void I2C::init_I2C(uint32_t clockFreq) {
 
 
 void I2C::start() {
-  transmitting = true;
   TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); //Sender start condition
-  while (!(TWCR & (1 << TWINT))) //venter indtil transmission ovre.
-    ;
+  while (!(TWCR & (1 << TWINT))) //venter indtil start er sendt.
+    {}
 }
 
-void I2C::write(unsigned char data) {
+void I2C::write(uint8_t data) {
   TWDR = data; //Data som bliver sendt
   TWCR = (1 << TWINT) | (1 << TWEN);
-  while (!(TWCR & (1 << TWINT))) //Venter indtil tranmssion ovre
-    ;
+  while (!(TWCR & (1 << TWINT))) //Venter indtil byte er sendt 
+    {}
 }
 
 //Sender stop condition
-void I2C::stop() { TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO); } 
+void I2C::stop() 
+{
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+  while (TWCR & (1 << TWSTO)) // vent indtil stop er gennemført
+    {}
+} 
 
 //Læser data fra I2C
-unsigned char I2C::read(unsigned char isLast) {
+uint8_t I2C::read(uint8_t isLast) 
+{
   // if not last byte to read
   if (isLast == 0) {
     TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA); //sender ack
   } else {
     TWCR = (1 << TWINT) | (1 << TWEN); //sender ikke ack
   }
-  while ((TWCR & (1 << TWINT)) == 0) {
-  }
+  while ((TWCR & (1 << TWINT)) == 0) 
+    {}
   return TWDR; //returnerer data i regiteret.
-} 
+}
