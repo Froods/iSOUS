1382db42d7c5f4302585aefee469972e4fe7d303
Viktor Førrisdahl <viktorkirkeby15@gmail.com>
2026-03-31 14:42:31 +0200

    tilføjet de stages, som vores motor skal have for at kunne dreje rundt

--- embedded/Window_Driver/src/window_driver.cpp
+++ embedded/Window_Driver/src/window_driver.cpp
@@ -1,1 +1,24 @@
 #include "window_driver.h"
+
+
+//stages to make the motor spin with the 4 coils
+//the motor spins when a coil i grounded
+const bool stages_O_Y_P_B[8][4] = {
+//step 1:
+{false, true, true, true},
+//step 2:
+{false, false, true, true},
+//step 3:
+{true, false, true, true},
+//step 4:
+{true, false, false, true},
+//step 5:
+{true, true, false, true},
+//step 6:
+{true, true, false, false},
+//step 7:
+{true, true, true, false},
+//step 8:
+{false, true, true, false},
+};
+
