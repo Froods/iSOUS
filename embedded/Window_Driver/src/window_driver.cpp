a26f8298519c1cf61e9fbe2fd5523cac22f6662f
Viktor Førrisdahl <viktorkirkeby15@gmail.com>
2026-04-03 14:36:57 +0200

    Tilføjet move-motor funktion, som kan få motoren til at køre enten fremad eller bagud baseret på en bool parameter. Også tilføjet lidt ekstra kommentarer på det første kode

--- embedded/Window_Driver/src/window_driver.cpp
+++ embedded/Window_Driver/src/window_driver.cpp
@@ -3,7 +3,6 @@
 
 //Defining motor pins
 //we are using pins 28 (O), 26 (Y), 24 (P), 22 (B)
-
 int motor_Pins[4] = {28,26,24,22};
 
 //init pins:
@@ -15,7 +14,8 @@ void init_Motor_pins() {
 }
 
 //stages to make the motor spin with the 4 coils
-//the motor spins when a coil i grounded
+//the motor spins when a coil is grounded
+//The O,Y,P,B stands for Orange, Yellow, Pink & Blue and is a refenrence to the cables on the ULN2003
 const bool stages_O_Y_P_B[8][4] = {
 //step 1:
 {false, true, true, true},
@@ -34,3 +34,33 @@ const bool stages_O_Y_P_B[8][4] = {
 //step 8:
 {false, true, true, false},
 };
+
+//A function to move the motor
+//takes a bool as a parameter for which direction. 
+//True = forward
+void move_Motor (bool direction) {
+    //This int keeps track of which stage the motor is in according to stages_O_Y_P_B
+    //it is made static to only be allorcated once (the first time the function is called)
+    //afterwards it keeps its assigned value even though the function is called a lot of times
+    static int current_Stage = 0;
+
+    // if direction is forward (true) then we want to go to the next step to go forward
+    //going from step 1 - 8 causes the motor to go forward
+    //going from step 8 -1 causes the motor to go backwards
+  if (direction) {
+    current_Stage++;
+  } else {
+    current_Stage--;
+  }
+  
+  //We only have steps from 1-8 (0-7) and therefore we want to keep the current_stage within that limit
+  //This way we avoid calling the stage_O_Y_P_B with a number, that does not match an array
+  if (current_Stage > 7) current_Stage = 0;
+  if (current_Stage < 0) current_Stage = 7;
+
+    // Lastly the four motor pins is set after the array in stage_O_Y_P_B
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(motor_Pins[i], stages_O_Y_P_B[current_Stage][i]);
+  }
+}
+
