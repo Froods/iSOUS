b8443f2e8265e08fdfff76bf3a60f6094c57e160
Viktor Førrisdahl <viktorkirkeby15@gmail.com>
2026-04-04 17:28:06 +0200

    Ændrede navnet overalt i koden fra step til stage, da det var mere passende

--- embedded/Window_Driver/src/window_driver.cpp
+++ embedded/Window_Driver/src/window_driver.cpp
@@ -17,43 +17,43 @@ void init_Motor_pins() {
 //the motor spins when a coil is grounded
 //The O,Y,P,B stands for Orange, Yellow, Pink & Blue and is a refenrence to the cables on the ULN2003
 const bool stages_O_Y_P_B[8][4] = {
-//step 1:
+//stage 1:
 {false, true, true, true},
-//step 2:
+//stage 2:
 {false, false, true, true},
-//step 3:
+//stage 3:
 {true, false, true, true},
-//step 4:
+//stage 4:
 {true, false, false, true},
-//step 5:
+//stage 5:
 {true, true, false, true},
-//step 6:
+//stage 6:
 {true, true, false, false},
-//step 7:
+//stage 7:
 {true, true, true, false},
-//step 8:
+//stage 8:
 {false, true, true, false},
 };
 
 //A function to move the motor
 //takes a bool as a parameter for which direction. 
 //True = forward
-void move_Motor (bool direction) {
+void move_Motor_1_stage (bool direction) {
     //This int keeps track of which stage the motor is in according to stages_O_Y_P_B
     //it is made static to only be allorcated once (the first time the function is called)
     //afterwards it keeps its assigned value even though the function is called a lot of times
     static int current_Stage = 0;
 
-    // if direction is forward (true) then we want to go to the next step to go forward
-    //going from step 1 - 8 causes the motor to go forward
-    //going from step 8 -1 causes the motor to go backwards
+    // if direction is forward (true) then we want to go to the next stage to go forward
+    //going from stage 1 - 8 causes the motor to go forward
+    //going from stage 8 -1 causes the motor to go backwards
   if (direction) {
     current_Stage++;
   } else {
     current_Stage--;
   }
   
-  //We only have steps from 1-8 (0-7) and therefore we want to keep the current_stage within that limit
+  //We only have stages from 1-8 (0-7) and therefore we want to keep the current_Stage within that limit
   //This way we avoid calling the stage_O_Y_P_B with a number, that does not match an array
   if (current_Stage > 7) current_Stage = 0;
   if (current_Stage < 0) current_Stage = 7;
@@ -63,4 +63,3 @@ void move_Motor (bool direction) {
     digitalWrite(motor_Pins[i], stages_O_Y_P_B[current_Stage][i]);
   }
 }
-
