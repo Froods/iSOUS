44a6f3953853674d3e85c03d85d3be144ea9515d
Viktor Førrisdahl <viktorkirkeby15@gmail.com>
2026-04-16 14:02:09 +0200

    made init function for PWM pin for curtain driver

--- embedded/Curtain_Driver/src/curtain_driver.cpp
+++ embedded/Curtain_Driver/src/curtain_driver.cpp
@@ -1,2 +1,11 @@
 #include "../include/curtain_driver.h"
 #include <Arduino.h>
+
+//Defining motor PWM pin:
+// we are using the PWM pin 2 (PE4, OC3B, INT4)
+//init pin:
+//Making motor pin become output:
+void init_Curtain_Motor_pin() {
+    pinMode(2, OUTPUT);
+}
+
